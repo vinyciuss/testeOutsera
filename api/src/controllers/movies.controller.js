9b02ecd9c737a95507b5cac05dea164c17deb540
Vinycius Souza <vinycius.sr@hotmail.com>
2024-12-10 22:55:06 -0300

    Update Main/Adição da função para obter o maior e menor intervalor entre prêmios

--- api/src/controllers/movies.controller.js
+++ api/src/controllers/movies.controller.js
@@ -1,8 +1,64 @@
 import MoviesService from "../services/movies.service.js";
 
+function findProducers(data) {
+    const producers = {};
+
+    data.forEach(item => {
+        if (item.winner === "yes") {
+            if (!producers[item.producers]) {
+                producers[item.producers] = [];
+            }
+            producers[item.producers].push(item.year);
+        }
+    });
+
+    let maxIntervalProducer = null;
+    let maxInterval = -Infinity; 
+    let minIntervalProducer = null;
+    let minInterval = Infinity;  
+
+    for (let producer in producers) {
+        const years = producers[producer].sort((a, b) => a - b); 
+        
+        if (years.length > 1) {
+            for (let i = 1; i < years.length; i++) {
+                const interval = years[i] - years[i - 1];
+
+                if (interval > maxInterval) {
+                    maxInterval = interval;
+                    maxIntervalProducer = producer;
+                }
+
+                if (interval < minInterval) {
+                    minInterval = interval;
+                    minIntervalProducer = producer;
+                }
+            }
+        }
+    }
+
+    return {
+        maxIntervalProducer,
+        maxInterval,
+        minIntervalProducer,
+        minInterval
+    };
+}
+
 async function getMovies(req, res, next){
     try {
-        res.send(await MoviesService.getMovies());
+
+        const result = findProducers(await MoviesService.getMovies())
+
+        // res.send(findProducers(await MoviesService.getMovies()));
+
+        
+        res.send({titulos:[{maior: result.maxIntervalProducer,
+                  intervalo: result.maxInterval},
+                  {menor: result.minIntervalProducer,
+                  intervalo: result.minInterval}]});
+
+        // res.send(await MoviesService.getMovies());
     } catch (err) {
         next(err);
     }
